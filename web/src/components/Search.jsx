00536fefa66702a8d1a48fd7522673a86535345f
rahulmadduluri <rahulmadduluri@gmail.com>
2019-03-21 19:37:46 -0700

    Formatting school search results

--- web/src/components/Search.jsx
+++ web/src/components/Search.jsx
@@ -3,6 +3,12 @@ import './Search.css';
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import Navbar from './navbar.jsx';
+import LocationIcon from '../location_icon.png';
+import ISAPayment from '../isa_payment.png';
+import UpfrontPayment from '../upfront_payment.png';
+import LengthIcon from '../length_icon.png';
+import SalaryIcon from '../salary_icon.png';
+import JobPlacementIcon from '../job_placement_icon.png';
 
 class Search extends Component {
 
@@ -48,7 +54,7 @@ class Search extends Component {
           }}
         </Query>      
       </div>
-    );
+    )
   }
 }
 
@@ -56,26 +62,17 @@ const List = ({ schools }) => (
   <div>
     <div className="list">
       {
-        schools.map(({ uuid, name, avgGraduateSalary, jobPlacementRate, lengthInWeeks, isOnline, photoURI, basePrice, paymentType, tracks, locations }) => (
+        schools.map(({ uuid, name, avgGraduateSalary, jobPlacementRate, lengthInWeeks, isOnline, photoURI, basePrice, paymentType, tracks, campusLocations }) => (
           <div className="list-row" key={uuid}>
             <div className="schoolInfoWrapper">
               <div className="name">
                 {name}
               </div>
-              <div className="location">
-              </div>
-              <div className="price">
-                ${basePrice}
-              </div>
-              <div className="length">
-                {lengthInWeeks} weeks
-              </div>
-              <div className="graduateSalary">
-                Average Graduate Salary ${avgGraduateSalary}
-              </div>
-              <div className="jobPlacementRate">
-                Job Placement Rate {jobPlacementRate}%
-              </div>
+              <LocationBar isOnline={isOnline} />
+              <PriceBar basePrice={basePrice} paymentType={paymentType} />
+              <LengthBar length={lengthInWeeks}/>
+              <SalaryBar avgGraduateSalary={avgGraduateSalary} />
+              <JobPlacementBar jobPlacementRate={jobPlacementRate} />
             </div>
           </div>
         ))
@@ -83,5 +80,72 @@ const List = ({ schools }) => (
     </div>
   </div>
 );
+
+function LocationBar(isOnlineWrapper) {
+  if (isOnlineWrapper.isOnline === true) {
+    return (
+      <div className="location">
+        <img src={LocationIcon} alt="Location"/>
+        <div className="campusLocations">Online</div>
+      </div>
+    );
+  }
+  return (
+    <div className="location">
+      <img src={LocationIcon} alt="Location"/>
+      <div className="campusLocations">Not Online</div>
+    </div>
+  );
+};
+
+function PriceBar(priceWrapper) {
+  let paymentType;
+
+  if (priceWrapper.paymentType === "ISA") {
+    paymentType = <img src={ISAPayment} alt="Payment Type"/>;
+  } else {
+    paymentType = <img src={UpfrontPayment} alt="PaymentType"/>;
+  }
+
+  return (
+    <div className="price">
+      <div className="basePrice">
+        ${priceWrapper.basePrice}
+      </div>
+      <div className="paymentType">
+        {paymentType}
+      </div>
+    </div>
+  );
+};
+
+function LengthBar(lengthWrapper) {
+  return (
+    <div className="length">
+      <img src={LengthIcon} alt="Length"/>
+      <div className="lengthInWeeks">{lengthWrapper.length} weeks</div>
+    </div>
+  );
+};
+
+function SalaryBar(salaryWrapper) {
+  return (
+    <div className="salary">
+      <img src={SalaryIcon} alt="Salary"/>
+      <div className="avgGraduateSalaryLabel">Average Graduate Salary</div>
+      <div className="avgGraduateSalary">${salaryWrapper.avgGraduateSalary}</div>
+    </div>
+  );
+};
+
+function JobPlacementBar(jobPlacementWrapper) {
+  return (
+    <div className="jobPlacement">
+      <img src={JobPlacementIcon} alt="Job Placement"/>
+      <div className="jobPlacementRateLabel">Job Placement Rate</div>
+      <div className="jobPlacementRate">{jobPlacementWrapper.jobPlacementRate}%</div>
+    </div>
+  );
+};
 
 export default Search;
--- web/src/index.js
+++ web/src/index.js
@@ -5,35 +5,10 @@ import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "react-apollo";
-import gql from "graphql-tag";
-
 
 const client = new ApolloClient({
 	uri: "api"
 });
-
-const filtersQuery = gql`
-  query GetFilters {
-    filters {
-      tracks {
-        uuid
-        name
-      }
-      locations {
-        uuid
-        city
-        country
-      }
-      paymentTypes
-    }
-  }
-`;
-
-client
-  .query({
-    query: filtersQuery
-  })
-  .then(result => console.log(result.data));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
