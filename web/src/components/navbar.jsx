6be7d635d3bb53cabce32a9c472f800500ff43cc
Yogi Seetharaman <yogi94@gmail.com>
2019-03-27 22:23:39 -0700

    Scaffold bulma navbar

--- web/src/components/navbar.jsx
+++ web/src/components/navbar.jsx
@@ -1,45 +1,73 @@
 import React from 'react';
 import './navbar.css';
-import SmartSearchBar from "./searchbar.jsx";
-import raftSquare from "../raft_square.png";
+import SmartSearchBar from './searchbar.jsx';
+import raftSquare from '../raft_square.png';
 
 class Navbar extends React.Component {
+  constructor() {
+    super();
 
-  onSearch = (searchText) => {
-  };
+    this.state = {
+      hamburgerActive: false,
+    };
 
-  onTapLink = () => {
+    this.toggleHamburgerMenu = this.toggleHamburgerMenu.bind(this);
+  }
+  onSearch = searchText => {};
 
-  };
+  onTapLink = () => {};
+  toggleHamburgerMenu() {
+    this.setState({
+      hamburgerActive: !this.state.hamburgerActive,
+    });
+  }
 
   render() {
     return (
-      <nav>
-        <div className="navWide">
-          <div className="raftSquare">
-            <img src={raftSquare} alt="Logo"/>
-          </div>
-          <div className="searchBar">
-            <SmartSearchBar />
-          </div>
-          <div className="navLinks">
-            <a href="http://www.google.com">Write a Review</a>
-            <a href="http://www.google.com">Help</a>
-            <a href="http://www.google.com">About Us</a>
-          </div>
-          <div className="menuLinks">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            <img src={raftSquare} alt="Logo" />
+          </a>
 
-            <input type="checkbox" />
-            
-            <span></span>
-            <span></span>
-            <span></span>
-            
-            <ul className="menu">
-              <a href="#"><li>Home</li></a>
-              <a href="#"><li>About</li></a>
-              <a href="#"><li>Info</li></a>
-            </ul>
+          <a
+            onClick={this.toggleHamburgerMenu}
+            role="button"
+            className={`navbar-burger burger ${
+              this.state.hamburgerActive ? 'is-active' : ''
+            }`}
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarItems"
+          >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+
+        <div
+          id="navbarItems"
+          className={`navbar-menu ${
+            this.state.hamburgerActive ? 'is-active' : ''
+          }`}
+        >
+          <div className="navbar-start">
+            <SmartSearchBar />
+            <a className="navbar-item">Write a Review</a>
+          </div>
+
+          <div className="navbar-end">
+            <a className="navbar-item">About Us</a>
+            <a className="navbar-item">Help</a>
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a className="button is-light">Log in</a>
+              </div>
+            </div>
           </div>
         </div>
       </nav>
