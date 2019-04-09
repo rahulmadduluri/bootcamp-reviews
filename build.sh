4962e37d0b052d6a161aefe24becad558d42881c
rahulmadduluri <rahulmadduluri@gmail.com>
2019-04-08 18:29:38 -0700

    add aws config files

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash 
+set -xe
+
+GOPATH="/tmp/go"
+APP_BUILD_DIR="$(pwd)/raft-build"  # We will build the app here
+APP_STAGING_DIR=$(pwd)                        # Current directory
+
+# Remove the $APP_BUILD_DIR just in case it was left behind in a failed build.
+rm -rf $APP_BUILD_DIR
+
+# Setup the application directory
+mkdir -p $APP_BUILD_DIR
+
+# cp all files to $APP_BUILD_DIR
+cp -r ./ $APP_BUILD_DIR
+cd $APP_BUILD_DIR
+cd ./backend/app
+
+# install packages and dependencies
+go mod init github.com/rahulmadduluri/raft-education/backend/app
+
+# build command
+go build -o application main/main.go
+
+# Modify permissons to make the binary executable.
+chmod +x application
+
+# Move the binary back to staging dir.
+mkdir "$APP_STAGING_DIR/backend/app/bin"
+mv application "$APP_STAGING_DIR/backend/app/bin"
+
+# Clean up.
+rm -rf $APP_BUILD_DIR
