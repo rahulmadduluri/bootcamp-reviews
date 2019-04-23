71fa03981719914cde917592d4123fa66d602fb1
rahulmadduluri <rahulmadduluri@gmail.com>
2019-04-22 19:17:48 -0700

    format which school did you attend?

--- web/src/components/WriteReview/WriteReview.jsx
+++ web/src/components/WriteReview/WriteReview.jsx
@@ -3,36 +3,83 @@ import Navbar from '../navbar.jsx';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import './WriteReview.css';
+import SchoolLogo from '../Common/SchoolLogo';
 
 class WriteReview extends Component {
 
-  // handlers
+  // school handlers
+
+  // set search flag to true on button press. when results come back 'update' and set flag to false
   didSearchForSchools = () => {
   	this.setState({ shouldFetchSchools: true });
-  };
-  handleUpdateSchoolSearch = (event) => {
-  	this.setState({ schoolSearchText: event.target.value });
   };
   updateSchoolResults = (schools) => {
   	this.setState({ schoolResults: schools, shouldFetchSchools: false });
   };
-  handleSelectSchool = (event) => {
-  	this.setState({ schoolUUID: event.target.value });
+  handleUpdateSchoolSearch = (event) => {
+  	this.setState({ schoolSearchText: event.target.value });
   };
+  handleSelectSchoolButtonPress = () => {
+  	const isActive = this.state.schoolDropdownActive;
+  	this.setState({ schoolDropdownActive: !isActive });
+  };
+  handleSelectSchool = (school) => {
+  	const isActive = this.state.schoolDropdownActive;
+  	this.setState({ schoolDropdownActive: !isActive, schoolUUID: school.uuid, selectedSchool: school });
+  };
+
   handleDidGraduate = (event) => {
-  	console.log(event.target);
   	if (event.target.id === "didGraduateYes") {
   		this.setState({ didGraduate: true });
   	} else {
   		this.setState({ didGraduate: false });
   	}
   };
+  handleSelectSchoolGradMonth = (event) => {
+  	if (event.target.value === 'none') {
+  		this.setState({ schoolGraduationMonth: null });
+  	} else {
+  		this.setState({ schoolGraduationMonth: event.target.value });
+  	}
+  };
+  handleSelectSchoolGradYear = (event) => {
+  	if (event.target.value === 'none') {
+  		this.setState({ schoolGraduationYear: null });
+  	} else {
+  		this.setState({ schoolGraduationYear: event.target.value });
+  	}
+  };
+
+  // school ratings
+  handleTeachingRating = (score) => {
+  	this.setState({ teachingScore: score });
+  };
+
+  // job handlers
+  handleSelectJobStartMonth = (event) => {
+  	if (event.target.value === 'none') {
+  		this.setState({ jobStartMonth: null });
+  	} else {
+  		this.setState({ jobStartMonth: event.target.value });
+  	}
+  };
+  handleSelectJobStartYear = (event) => {
+  	if (event.target.value === 'none') {
+  		this.setState({ jobStartYear: null });
+  	} else {
+  		this.setState({ jobStartYear: event.target.value });
+  	}
+  };
 
   state = {
+  	// state of page
   	shouldFetchSchools: true,
   	schoolSearchText: "",
   	schoolResults: [],
+  	schoolDropdownActive: false,
+  	selectedSchool: null,
 
+  	// review params
   	allText: null,
   	teachingScore: null,
   	courseworkScore: null,
@@ -46,8 +93,10 @@ class WriteReview extends Component {
 	schoolUUID: null,
 	schoolLocationUUID: null,
 	jobLocationUUID: null,
-	schoolGraduationDate: null,
-	jobStartDate: null
+	schoolGraduationMonth: null,
+	schoolGraduationYear: null,
+	jobStartMonth: null,
+	jobStartYear: null
   };
 
   render() {
@@ -66,27 +115,43 @@ class WriteReview extends Component {
 						<div className="reviewInfoPrivacyStatement">NOTE: We take your privacy seriously. This review will be completely <strong>ANONYMOUS</strong> to both the school and prospective students.</div>
 					</div>
 				</div>
-				<div className="writeReviewScoreWrapper">
+				<div className="writeReviewAttendanceWrapper">
 					<div className="defaultContainer column is-three-fifths">
 						{
-							this.schoolReviewInfo()
+							this.schoolAttendanceInfo()
 						}
 					</div>
 				</div>
-				<div className="submitReviewWrapper">
-					<div className="defaultContainer column is-three-fifths">
-						{
-							this.submitInfo()
-						}
-					</div>
-				</div>
+				{
+					(this.state.didGraduate != null) ? (
+						<div className="writeReviewScoreWrapper">
+							<div className="defaultContainer column is-three-fifths">
+								{
+									this.schoolReview()
+								}
+							</div>
+						</div>
+					) : <div/>
+				}
+				{
+					(this.state.didGraduate != null) ? (
+						<div className="submitReviewWrapper">
+							<div className="defaultContainer column is-three-fifths">
+								{
+									this.submitInfo()
+								}
+							</div>
+						</div>
+					) : <div/>
+				}
 			</div>
 		</div>
     );
   }
 
   // html components
-  schoolReviewInfo = () => {
+
+  schoolAttendanceInfo = () => {
 	const schoolsQuery = gql`
 	  query GetSchools($searchParams: SchoolSearchParams!) {
 	    schools(params: $searchParams) {
@@ -120,80 +185,143 @@ class WriteReview extends Component {
 	  }
 	`;
 
+	const { schoolSearchText, shouldFetchSchools, schoolResults, schoolUUID, didGraduate, schoolDropdownActive, selectedSchool } = this.state;
+
 	const searchParams = {
       pageNumber: 0,
-      searchText: this.state.schoolSearchText
+      searchText: schoolSearchText
 	};
-
 
   	return (
 	  <div>
 		  <div className="field">
 	        <label className="label"><div className="reviewFieldLabel">Which school did you attend?</div></label>
-	        <div className="field-body">
-	        	<div className="field has-addons">
-					<div className="control">
-						{
-							<input className="input" type="text" placeholder="Enter School Name" onChange={this.handleUpdateSchoolSearch} />
-						}
-					</div>
-					<div className="control">
-						<a className="button is-info" onClick={ this.didSearchForSchools }>
-						  Search
-						</a>
-					</div>
-				</div>
-	        </div>
+
+	        {
+	          selectedSchool ?
+              <div className="media">
+                <div className="media-left image">
+                  <SchoolLogo photoURI={selectedSchool.photoURI} />
+                </div>
+                <div className="media-content">
+                  <div className="name">{selectedSchool.name}</div>
+                </div>
+                <br/><br/>
+              </div> : <div/>
+	        }
+
+
+	        {
+	        	selectedSchool ? <div/> :
+			        <div className="field-body">
+			        	<div className="field has-addons">
+							<div className="control">
+								{
+									<input className="input" type="text" placeholder="Enter School Name" onChange={this.handleUpdateSchoolSearch} />
+								}
+							</div>
+							<div className="control">
+								<a className="button is-info" onClick={ this.didSearchForSchools }>
+								  Search
+								</a>
+							</div>
+						</div>
+			        </div>
+	        }
 	    </div>
 
 	    {
-	    	this.state.shouldFetchSchools ?
-		       <Query
-		          query={schoolsQuery}
-		          variables={{ searchParams: searchParams }}
-		          onCompleted={data => this.updateSchoolResults(data.schools.schoolResults)}
-		        >
-		          {({ loading, error, data }) => {
-		            if (loading) return <p></p>;
-		            if (error) return <p>Error :(</p>;
-		          	return <div/>;
-		          }}
-		        </Query> : (
-					<div className="field">
-					  <div className="field-body">
-						  <div className="control">
-						  	<SchoolDropdown schools={this.state.schoolResults} handleSelectSchool={this.handleSelectSchool} />
+	    	selectedSchool ? <div/> : (
+		    	// Dropdown -- Which school did you go to?
+		    	shouldFetchSchools ?
+			       <Query
+			          query={schoolsQuery}
+			          variables={{ searchParams: searchParams }}
+			          onCompleted={data => this.updateSchoolResults(data.schools.schoolResults)}
+			        >
+			          {({ loading, error, data }) => {
+			            if (loading) return <p></p>;
+			            if (error) return <p>Error :(</p>;
+
+
+			          	return (
+							<div className="field">
+							  <div className="field-body">
+								  <div className="control">
+								  	<SchoolDropdown 
+								  		schools={data.schools.schoolResults} 
+								  		handleSelectSchoolButtonPress={this.handleSelectSchoolButtonPress} 
+								  		handleSelectSchool={this.handleSelectSchool} 
+								  		dropdownActive={schoolDropdownActive} />
+								  </div>
+							  </div>
+							</div>
+			          	);
+			          }}
+			        </Query> : (
+						<div className="field">
+						  <div className="field-body">
+							  <div className="control">
+							  	<SchoolDropdown 
+							  		schools={this.state.schoolResults} 
+							  		handleSelectSchoolButtonPress={this.handleSelectSchoolButtonPress} 
+							  		handleSelectSchool={this.handleSelectSchool} 
+							  		dropdownActive={schoolDropdownActive} />
+							  </div>
 						  </div>
-					  </div>
-					</div>
-		        )
+						</div>
+			        )
+		    )
 	    	
 	    }
 
-		<div className="field">
-		  <label className="label"><div className="reviewFieldLabel">Did you graduate?</div></label>
-		  <div className="field-body">
-				<div className="field">
-				  <input className="is-checkradio" id="didGraduateYes" type="radio" name="exampleRadioDefault" onChange={this.handleDidGraduate} />
-				  <label htmlFor="didGraduateYes">Yes</label>
-				  <input className="is-checkradio" id="didGraduateNo" type="radio" name="exampleRadioDefault" onChange={this.handleDidGraduate} />
-				  <label htmlFor="didGraduateNo">No</label>
-				</div>
-		    </div>
-		</div>
+		{
+			// Did Graduate?
+			schoolUUID ?
+			<div className="field">
+			  <label className="label"><div className="reviewFieldLabel">Did you graduate?</div></label>
+			  <div className="field-body">
+					<div className="field">
+					  <input className="is-checkradio" id="didGraduateYes" type="radio" name="exampleRadioDefault" onChange={this.handleDidGraduate} />
+					  <label htmlFor="didGraduateYes">Yes</label>
+					  <input className="is-checkradio" id="didGraduateNo" type="radio" name="exampleRadioDefault" onChange={this.handleDidGraduate} />
+					  <label htmlFor="didGraduateNo">No</label>
+					</div>
+			    </div>
+			</div>
+			: <div/>
+		}
 
 		{
-			this.state.didGraduate ? 
+			// When did you graduate?
+			didGraduate ? 
 			<div className="field">
 			  <label className="label"><div className="reviewFieldLabel">When did you graduate?</div></label>
 			  <div className="field-body">
-					<div className="field">
-					</div>
+				  <div className="control">
+				  	<MonthDropdown schools={schoolResults} handleSelectMonth={this.handleSelectSchoolGradMonth} />
+				  	<YearDropdown schools={schoolResults} handleSelectYear={this.handleSelectSchoolGradYear} />
+				  </div>
+				  <div className="control">
+				  </div>
 			    </div>
 			</div> : <div/>
 		}
 
 	  </div>  	
+  	);
+  };
+
+  schoolReview = () => {
+  	return (
+		<div className="field">
+		  <label className="label"><div className="reviewFieldLabel">How effective was the teaching?</div></label>
+		  <div className="field-body">
+			  <div className="control">
+				<input className="slider is-fullwidth" step="1" min="0" max="10" value={this.state.teachingScore ? this.state.teachingScore : 5} type="range" onChange={this.handleTeachingRating} />
+			  </div>
+		    </div>
+		</div>
   	);
   };
 
@@ -209,18 +337,70 @@ class WriteReview extends Component {
   };
 }
 
-const SchoolDropdown = ({ schools, handleSelectSchool }) => (
-  <div className="select">
-      {
-		<select onChange={handleSelectSchool}>
-		  <option>Select School</option>
+const SchoolDropdown = ({ schools, handleSelectSchoolButtonPress, handleSelectSchool, dropdownActive }) => (
+	<div className={dropdownActive ? "dropdown is-active" : "dropdown"}>
+	  <div className="dropdown-trigger">
+	    <button className="button" aria-haspopup="true" aria-controls="dropdown-menu2" onClick={handleSelectSchoolButtonPress}>
+	      <span>Select School</span>
+	      <span className="icon is-small">
+	        <i className="fas fa-angle-down" aria-hidden="true"></i>
+	      </span>
+	    </button>
+	  </div>
+	  <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+	    <div className="dropdown-content">
 		  {
 		      	schools.map(
 		        ({
 		          uuid,
-		          name
-		        }) => (
-		          <option key={uuid} value={uuid}>{name}</option>
+		          name,
+		          photoURI
+		        }, index) => (
+
+		          <a key={uuid} className="dropdown-item" value={uuid} onClick={() => handleSelectSchool(schools[index])} >
+	                  <div className="media">
+	                    <div className="media-left image">
+	                      <SchoolLogo photoURI={photoURI} />
+	                    </div>
+	                    <div className="media-content">
+	                      <div className="name">{name}</div>
+	                    </div>
+	                  </div>
+	               </a>
+		        )
+		      )
+		  }
+	    </div>
+	  </div>
+	</div>
+);
+
+const MonthDropdown = ({ handleSelectMonth }) => (
+  <div className="select">
+      {
+		<select onChange={handleSelectMonth}>
+		  <option value={'none'}>Select Month</option>
+		  {
+		      	["January","February","March","April","May","June","July","August","September","October","November","December"].map(
+		        (month, index) => (
+		          <option key={"month:"+index} value={index+1}>{month}</option>
+		        )
+		      )
+		  }
+		</select>
+    }
+  </div>
+);
+
+const YearDropdown = ({ handleSelectYear }) => (
+  <div className="select">
+      {
+		<select onChange={handleSelectYear}>
+		  <option value={'none'}>Select Year</option>
+		  {
+		      	["2013","2014","2015","2016","2017","2018","2019"].map(
+		        (year) => (
+		          <option key={"year:"+year} value={year}>{year}</option>
 		        )
 		      )
 		  }
