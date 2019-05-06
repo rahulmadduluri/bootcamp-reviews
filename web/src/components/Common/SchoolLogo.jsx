8527904d0f228e9ea08a1154494291b353cec6b2
rahulmadduluri <rahulmadduluri@gmail.com>
2019-05-05 17:49:11 -0700

    tryagain3

--- web/src/components/Common/SchoolLogo.jsx
+++ web/src/components/Common/SchoolLogo.jsx
@@ -5,9 +5,9 @@ function SchoolLogo({ photoURI }) {
   if (process.env.NODE_ENV === 'development') {
     url = window.location.protocol + '//localhost:8080/s3/schools/' + photoURI;
   } else {
-    console.log("LOL");
-    url = '/s3/schools/' + photoURI;
-    console.log(url);
+    url =
+      '/s3/schools/' +
+      photoURI;
   }
   return <img src={url} alt="SchoolLogo" />;
 }
